111ebe1b93a98536068a79a78a0a6e05e2921c90
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2021-10-12 16:27:17 +0200

    Added break to prevent a bug, updated comments

--- main.go
+++ main.go
@@ -10,12 +10,13 @@ import (
 )
 
 const (
+	// * Buffer size for the file
 	maxSize = 1024 * 1024
 )
 
 var (
 	results      = []string{}
-	keywords     = []string{"github", "aws", "//", "/*", "*/"}
+	keywords     = []string{"github", ".com", "//", "/*", "*/"}
 	file_formats = []string{".txt", ".gitignore", ".ts", ".js", ".sum", ".mod", ".md", ".sh", ".json", ".yaml", ".lock", ".tf", ".go", ".py", ".groovy", ".csh", ".html", ".css"}
 )
 
@@ -30,10 +31,10 @@ func main() {
 	log.Println("Searching dir: ", dir)
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			log.Println(err)
+			log.Fatalln(err)
 		}
 		if !info.IsDir() {
-			// * Add the task to the wait queue and read file
+			// * If the file isn't diretory and matches the file format read the file
 			for _, ext := range file_formats {
 				if filepath.Ext(path) == ext {
 					ReadFile(path)
@@ -63,7 +64,8 @@ func ReadFile(path string) {
 		// * Check the line for keywords
 		for _, i := range keywords {
 			if strings.Contains(line, i) {
-				results = append(results, line)
+				results = append(results, line+"; "+path)
+				break
 			}
 		}
 	}
@@ -73,6 +75,7 @@ func ReadFile(path string) {
 }
 
 func writeToFile() {
+	// TODO: Make write to file method not rely on finishing the search
 	file, err := os.Create("result.txt")
 	if err != nil {
 		panic(err)
@@ -83,3 +86,5 @@ func writeToFile() {
 		file.WriteString(line + "\n")
 	}
 }
+
+// TODO: Create a Python script that parses the result.txt
